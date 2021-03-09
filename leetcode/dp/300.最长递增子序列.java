66de9e168beb8a0ba62b599fcedf7e080f7eaf92
BUPThwt <hwttumtum@163.com>
2021-03-09 08:59:16 +0800

    dp

---
+++ leetcode/dp/300.最长递增子序列.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=java
+ *
+ * [300] 最长上升子序列
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums.length <= 1){
+            return nums.length;
+        }
+        int result = 0;
+        int[] dp = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            dp[i] = 1;
+        }
+        for(int i = 1; i < nums.length; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+        return result;
+    }
+}
+// @lc code=end
+
