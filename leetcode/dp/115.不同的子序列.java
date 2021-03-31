50b4f4935f65ba81ab88a607a40d10cf44e4ef9b
BUPThwt <hwttumtum@163.com>
2021-03-31 13:50:27 +0800

    dp

---
+++ leetcode/dp/115.不同的子序列.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=115 lang=java
+ *
+ * [115] 不同的子序列
+ */
+
+// @lc code=start
+class Solution {
+    public int numDistinct(String s, String t) {
+        //dp[i][j]表示 以i-1结尾的s中以j-1结尾的t的个数
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        for(int i = 0; i <= s.length(); i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1; i <= s.length(); i++){
+            for(int j = 1; j <= t.length(); j++){
+                if(s.charAt(i - 1) == t.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[s.length()][t.length()];
+    }
+}
+// @lc code=end
+
