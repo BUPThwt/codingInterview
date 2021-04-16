d1e226b479dff1e5176490254374b3c1ddc0742c
BUPThwt <hwttumtum@163.com>
2021-04-16 17:49:22 +0800

    pickone

---
+++ leetcode/pickone_April/239.滑动窗口最大值.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+import apple.laf.JRSUIConstants.Widget;
+
+/*
+ * @lc app=leetcode.cn id=239 lang=java
+ *
+ * [239] 滑动窗口最大值
+ */
+
+// @lc code=start
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums.length < 2){
+            return nums;
+        }
+        LinkedList<Integer> queue = new LinkedList<>();
+        int[] result = new int[nums.length - k + 1];
+        for(int i = 0; i < nums.length; i++){
+            while(!queue.isEmpty() && nums[queue.peekLast()] < nums[i]){
+                queue.pollLast();
+            }
+            queue.add(i);
+            if(i - k >= queue.peek()){
+                queue.poll();
+            }
+            if(i - k + 1 >= 0){
+                result[i - k + 1] = nums[queue.peek()];
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
+
