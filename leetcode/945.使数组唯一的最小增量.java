f2591cded0032cdfdbe07e4da6ee6bc3a4ec5889
BUPThwt <hwttumtum@163.com>
2021-04-25 11:18:32 +0800

    pickone

---
+++ leetcode/945.使数组唯一的最小增量.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=945 lang=java
+ *
+ * [945] 使数组唯一的最小增量
+ */
+
+// @lc code=start
+class Solution {
+    public int minIncrementForUnique(int[] A) {
+        int ans = 0;
+        Arrays.sort(A);
+        for(int i = 1; i < A.length; i++){
+            if(A[i] <= A[i - 1]){
+                int pre = A[i];
+                A[i] = A[i - 1] + 1;
+                ans += A[i] - pre;
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
