f7691c4b03768434e27f1ed385fadc6bb968714d
BUPThwt <hwttumtum@163.com>
2021-05-14 14:32:34 +0800

    pickone

---
+++ leetcode/pickone_May/160.相交链表.java
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=java
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        int lenA = 0;
+        int lenB = 0;
+        while(curA != null){
+            lenA++;
+            curA = curA.next;
+        }
+        while(curB != null){
+            lenB++;
+            curB = curB.next;
+        }
+        curA = headA;
+        curB = headB;
+        if(lenB > lenA){
+            int temp = lenA;
+            lenA = lenB;
+            lenB = temp;
+            ListNode tempNode = curA;
+            curA = curB;
+            curB = tempNode;
+        }
+        int gap = lenA - lenB;
+        while(gap-- > 0){
+            curA = curA.next;
+        }
+        while(curA != null){
+            if(curA == curB){
+                return curA;
+            }
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return null;
+    }
+
+}
+// @lc code=end
+
