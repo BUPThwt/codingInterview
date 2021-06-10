dea2f30a908dc836a2e239faa89b5507fd1e5f2c
BUPThwt <hwttumtum@163.com>
2021-06-10 22:50:13 +0800

    dp bag

--- leetcode/pickone_june/70.爬楼梯.java
+++ leetcode/pickone_june/70.爬楼梯.java
@@ -6,18 +6,33 @@
 
 // @lc code=start
 class Solution {
-    public int climbStairs(int n) {
+    // public int climbStairs(int n) {
+    //     int[] dp = new int[n + 1];
+    //     dp[0] = 1;
+    //     for(int i = 1; i <= n; i++){
+    //         for(int j = 1; j <= 2; j++){
+    //             if(i - j >= 0){
+    //                 dp[i] += dp[i - j];
+    //             }
+    //         }
+    //     }
+    //     return dp[n];
+    // }
+
+    public int climbStairs(int n){
         int[] dp = new int[n + 1];
         dp[0] = 1;
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= 2; j++){
-                if(i - j >= 0){
+                if(i >= j){
                     dp[i] += dp[i - j];
                 }
             }
         }
         return dp[n];
     }
+
+
 }
 // @lc code=end
 
