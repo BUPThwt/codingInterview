c95eacfe5fa3bef9c4e44c174ebf067f31b3f5a7
BUPThwt <hwttumtum@163.com>
2021-06-21 16:05:58 +0800

    real

--- leetcode/146.lru-缓存机制.java
+++ leetcode/146.lru-缓存机制.java
@@ -1,3 +1,5 @@
+import java.util.LinkedHashMap;
+
 /*
  * @lc app=leetcode.cn id=146 lang=java
  *
@@ -7,17 +9,37 @@
 // @lc code=start
 class LRUCache {
 
-    
+    int cap;
+    LinkedHashMap<Integer, Integer> cache = new LinkedHashMap<>();
     public LRUCache(int capacity) {
-
+        cap = capacity;
     }
     
     public int get(int key) {
-
+        if(!cache.containsKey(key)){
+            return -1;
+        }
+        makeRecently(key);
+        return cache.get(key);    
     }
     
     public void put(int key, int value) {
+        if(cache.containsKey(key)){
+            cache.put(key, value);
+            makeRecently(key);
+            return;
+        }
+        if(cache.size() >= cap){
+            int oldKey = cache.keySet().iterator().next();
+            cache.remove(oldKey);
+        }
+        cache.put(key, value);
+    }
 
+    private void makeRecently(int key){
+        int val = cache.get(key);
+        cache.remove(key);
+        cache.put(key, val);
     }
 }
 
